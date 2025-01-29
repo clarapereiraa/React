c5b50258aa777a7a03f7d1d8e08c2c784422266d
clarapereiraa <clasipereira@gmail.com>
2025-01-29 11:26:37 -0300

    primeira atividade pratic react e componet

---
+++ React29-01/meuSiteBasico/src/App.jsx
@@ -0,0 +1,10 @@
+import Component1 from "./Component";
+function App() {
+  return (
+  <div>
+    <h1 style={{padding: "10px 20px", color: "blue"}}>Component que pode ser reutilizado</h1>
+  </div>
+  );
+}
+
+export default App;
